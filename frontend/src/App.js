4336aea7c57ee3c971d85da1e2f2cdfafccfc13c
tharunvikram71 <vrajamani741@gmail.com>
2025-07-15 19:32:03 +0530

    new commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,8 @@ import RegisterForm from "./Pages/Login&register/Register";
 import { UserProvider } from "./context/userContext.js";
 import Developers from "./Pages/Developers.js";
 import { ToastContainer } from "react-toastify";
+import Plans from "./Pages/Plans.js";
+import Settings from "./Pages/Settings.js";
 
 function App() {
   return (
@@ -20,6 +22,10 @@ function App() {
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/developers" element={<Developers />} />
+                    <Route path="/plans" element={<Plans />} />
+
+          <Route path="/settings" element={<Settings />} />
+
         </Route>
       </Routes>
     </UserProvider>
