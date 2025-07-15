8e421ee48aad519ff2ef2eea74e9af1bccd02043
saran247 <kirithiksaran@gmail.com>
2025-07-15 21:18:15 +0530

    new  commit

--- frontend/src/Pages/Business/BusinessDashboard.js
+++ frontend/src/Pages/Business/BusinessDashboard.js
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import { useNavigate } from "react-router-dom"
 const BusinessDashboard = () => {
+     const navigate = useNavigate();
   return (
-    <div>BusinessDashboard</div>
-  )
-}
+    <div className="container-fluid py-5">
+      <div className="row ">
+        <div className="col-12 col-md-8 col-lg-6  ">
+          <div className="d-flex flex-wrap gap-3 justify-content-center">
+            <button
+              className="btn btn-primary mb-3"
+              onClick={() => navigate("/addproduct")}
+            >
+              Add Product
+            </button>
 
-export default BusinessDashboard
+            <button
+              className="btn btn-primary mb-3"
+              onClick={() => navigate("/addemployee")}
+            >
+              Add Employee
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BusinessDashboard;
