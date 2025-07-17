b6de03d1d81706db6c05a36e3240c9e456073cdb
saran247 <kirithiksaran@gmail.com>
2025-07-17 18:30:16 +0530

    new commit

--- frontend/src/Pages/Dashboard/ManageBusiness.js
+++ frontend/src/Pages/Dashboard/ManageBusiness.js
@@ -13,52 +13,6 @@ const ManageBusiness = () => {
     (b) => b.businessId === selectedBusinessId
   );
   return (
-    // <div className="container-fluid py-2">
-    //   {/* Buttons Section */}
-    //   <div className="row justify-content-center">
-
-    //     <h3 className="text-center mb-5">
-    //       <strong>{selectedBusiness?.name} </strong>
-    //     </h3>
-    //     <div className="col-12">
-
-    //       <div className="row gy-2 justify-content-center">
-    //         <div className="col-auto">
-    //           <button
-    //             className="btn btn-primary w-100"
-    //             onClick={() => setSelectedForm("product")}
-    //           >
-    //             <i className="bi bi-plus-circle-fill fs-6 me-2"></i> Add Product
-    //           </button>
-    //         </div>
-    //         <div className="col-auto">
-    //           <button
-    //             className="btn btn-primary w-100"
-    //             onClick={() => setSelectedForm("employee")}
-    //           >
-    //             <i className="bi bi-person-add fs-6 me-2"></i> Add Employee
-    //           </button>
-    //         </div>
-
-    //         <div className="col-auto">
-    //           <button
-    //             className="btn btn-primary w-100"
-    //             onClick={() => setSelectedForm("trader")}
-    //           >
-    //             <i className="bi bi-tags-fill fs-6 me-2"></i> Add Trade Party
-    //           </button>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-
-    //   {/* Form Section - Render based on button clicked */}
-    //   <div className="row justify-content-center mt-4">
-    //     {selectedForm === "product" && <AddProduct />}
-    //     {selectedForm === "employee" && <AddEmployee />}
-    //     {selectedForm === "trader" && <AddTradeParty />}
-    //   </div>
-    // </div>
 
     <div className="container-fluid py-2">
       {/* Business Info Card */}
